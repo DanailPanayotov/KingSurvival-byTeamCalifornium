71c688d78d9ac5cc56b5fab967829f564eab22fa
Parushev <anton.parushev@gmail.com>
2013-05-14 17:05:50 +0300

    Changed the King and Pawn classes to derive from the abstract Figure class

--- King.cs
+++ King.cs
@@ -1,71 +1,14 @@
-// ********************************
-// <copyright file="King.cs" company="Team Californium">
-// Copyright (c) 2013 Team Californium. All rights reserved.
-// </copyright>
-//
-// ********************************
+using System;
+
 namespace KingSurvival
 {
-    using System;
-    using System.Linq;
-
-    //az probvam neshto
-
-    //proba
-    //trying again
-    /// <summary>
-    /// Class King which creates King objects with X and Y coordinates.
-    /// </summary>
-    public class King
+    class King
+        : Figure
     {
-        /// <summary>
-        /// Keeps X coordinate for the Pawn object.
-        /// </summary>
-        private int x;
-
-        /// <summary>
-        /// Keeps Y coordinate for the Pawn object.
-        /// </summary>
-        private int y;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="King" /> class 
-        /// with coordinates (0, 0).
-        /// </summary>
-        public King()
+        public King(int x, int y, char name)
+            : base(x, y, name)
         {
-            this.x = 0;
-            this.zda = 0;
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="King" /> class 
-        /// with coordinates (<paramref name="x"/>, <paramref name="y"/>).
-        /// </summary>
-        /// <param name="x">X coordinate for King object</param>
-        /// <param name="y">Y coordinate for King object</param>
-        public King(int x, int y)
-        {
-            this.x = x;
-            this.y = y;
-        }
-
-        /// <summary>
-        /// Gets or sets X coordinate.
-        /// </summary>
-        public int X
-        {
-            get { return this.x; }
-            set { this.x = value; }
-        }
-
-        /// <summary>
-        /// Gets or sets Y coordinate.
-        /// </summary>
-        public int Y
-        {
-            get { return this.y; }
-            set { this.y = value; }
+            this.DefaultMoves = new string[4] { "DL", "DR", "UL", "UR" };
         }
     }
 }
--- Pawn.cs
+++ Pawn.cs
@@ -1,67 +1,14 @@
-// ********************************
-// <copyright file="Pawn.cs" company="Team Californium">
-// Copyright (c) 2013 Team Californium. All rights reserved.
-// </copyright>
-//
-// ********************************
+using System;
+
 namespace KingSurvival
 {
-    using System;
-    using System.Linq;
-
-    /// <summary>
-    /// Class Pawn which creates Pawn objects with X and Y coordinates.
-    /// </summary>
-    public class Pawn
+    class Pawn
+        : Figure
     {
-        /// <summary>
-        /// Keeps X coordinate for the Pawn object.
-        /// </summary>
-        private int x;
-
-        /// <summary>
-        /// Keeps Y coordinate for the Pawn object.
-        /// </summary>
-        private int y;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Pawn" /> class 
-        /// with coordinates (0, 0).
-        /// </summary>
-        public Pawn()
+        public Pawn(int x, int y, char name)
+            : base(x, y, name)
         {
-            this.x = 0;
-            this.y = 0;
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Pawn" /> class 
-        /// with coordinates (<paramref name="x"/>, <paramref name="y"/>).
-        /// </summary>
-        /// <param name="x">X coordinate for Pawn object</param>
-        /// <param name="y">Y coordinate for Pawn object</param>
-        public Pawn(int x, int y)
-        {
-            this.x = x;
-            this.y = y;
-        }
-
-        /// <summary>
-        /// Gets or sets X coordinate.
-        /// </summary>
-        public int X
-        {
-            get { return this.x; }
-            set { this.x = value; }
-        }
-
-        /// <summary>
-        /// Gets or sets Y coordinate.
-        /// </summary>
-        public int Y
-        {
-            get { return this.y; }
-            set { this.y = value; }
+            this.DefaultMoves = new string[2] { "DR", "DL" };
         }
     }
 }
